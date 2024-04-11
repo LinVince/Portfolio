cd5f7709db138d4ec7d5992c9d8948606f800898
LinVince <vincejim91126@gmail.com>
2024-04-12 00:27:19 +0100

    add energy scenario explorer

--- portfolio/5.js
+++ portfolio/5.js
@@ -9,7 +9,7 @@ Section.appendChild(Container)
 
 titleText = document.createElement('div')
 titleText.setAttribute('class' , 'title')
-titleText.textContent="Python - Data Visualization/Processing"
+titleText.textContent="UX, Fullstack - Energy Transition Scenario Explorer"
 Container.appendChild(titleText)
 
 DescLabel = document.createElement('div')
@@ -20,7 +20,7 @@ Container.appendChild(DescLabel)
 
 DescText = document.createElement('div')
 DescText.setAttribute('class' , 'paragraph-light')
-DescText.textContent="Whether it's for my work or the course I taught, I needed to visualise data for stakeholders to comprehend the digital marketing performance or for students to learn what problems we can solve with data visualization. Here are the problem-solving examples and real-life use cases in terms of quantitative data processing and visualization. Some examples I showed to students were extracted from Dr. Angela Yu. And the major project is the integration of Google Analytics 4 and Python. I leveraged the API and used Python to automate the generation of commercial data reports. Here I will also show the challenges and how I ended up visualizing data in this way."
+DescText.textContent="The project was funded by the UK Engineering and Physical Sciences Research Council and executed by University College London (UCL). In this project, I co-worked with an outstanding research fellow who did research on energy transition. We together built an interactive web app for users to explore the energy transition scenarios so as to facilitate their policy making in relevant domains. "
 Container.appendChild(DescText)
 
 TimeLabel = document.createElement('div')
@@ -30,8 +30,18 @@ Container.appendChild(TimeLabel)
 
 TimeText = document.createElement('div')
 TimeText.setAttribute('class' , 'paragraph-light')
-TimeText.textContent="2021 to present"
+TimeText.textContent="2024 Jan - April"
 Container.appendChild(TimeText)
+
+ClientLabel = document.createElement('div')
+ClientLabel.setAttribute('class' , 'label')
+ClientLabel.textContent="client"
+Container.appendChild(ClientLabel)
+
+ClientText = document.createElement('div')
+ClientText.setAttribute('class' , 'paragraph-light')
+ClientText.textContent="University College London (UCL)"
+Container.appendChild(ClientText)
 
 AuthorLabel = document.createElement('div')
 AuthorLabel.setAttribute('class' , 'label')
@@ -40,124 +50,48 @@ Container.appendChild(AuthorLabel)
 
 AuthorText = document.createElement('div')
 AuthorText.setAttribute('class' , 'paragraph-light')
-AuthorText.textContent="YUEH LIN"
+AuthorText.textContent="Yueh, Leo"
 Container.appendChild(AuthorText)
 
 text = [
-  {
-  'tag':'div',
-  'class':'title',
-  'text':"Project at A Glance"},
-  {'tag':'div',
-   'class':'text',
-   'text':"1. Google Analytics 4 API empowered by Python"},
-   {'tag':'div',
-   'class':'text',
-   'text':"2. CSV Data Processing and Visualization"},
-   {'tag':'div',
-   'class':'text',
-   'text':"3. Conclusion"},
-  {
-  'tag':'div',
-  'class':'title',
-  'text':"1. Google Analytics 4 API empowered by Python"},
-  {'tag':'img','src':"../image/pythonxga4.jpg"},
-  {'tag':'div','class':'title','text':'Background'},
-  {'tag':'div',
-   'class':'text',
-   'text':"Analyzing commercial data on Google Analytics may be time-consuming. Calculation requires manual work. With the use of Python, I not only generate the report with just one click, but the flexibility allows me to cross-compare different types of data and efficiently extract the types of reports I need."},
-   {'tag':'div',
-   'class':'text',
-   'text':"Here I collaborate with an English school in Taiwan. I will showcase how the numbers of the official website visitors and conversions are visualized. I will mention the challenges I have faced and my considerations in the process of development as well."},
-   {'tag':'div','class':'title','text':'Commercial Data'},
-    {'tag':'div',
-   'class':'text',
-   'text':"It is common that enterprises or ecommerce stores use Google Analytics to track the website traffic and calculation the conversion rate. The English school also used Google Analytics to track the number of visitors. On the website, there are two conversion events. One of them is submitting the contact form, the other joining the official LINE account."},
-    {'tag':'div','class':'title','text':'Integration with Google Analytics 4 API'},
-    {'tag':'div',
-   'class':'text',
-   'text':"Google provides developers with GA4 API to play with data. According to the documentation, I can call the given function and set the parameters and metrics to retrieve the data I want."},
-   {'tag':'img','src':"../image/GA4_API.jpg"},
-   {'tag':'div',
-   'class':'text',
-   'text':"Here I use Jupyter Notebook to render the charts and tables. I also modified the function, defining one for the number of visits and the other for the number of conversions."},
-   {'tag':'img','src':"../image/jupyter.jpg"},
-  {'tag':'div',
-   'class':'text',
-   'text':"After configuring the period of time I expect to look into, I get the raw data from Google Analytics Data API. Then I have to write codes to define the data structure and extract the data I need for further analysis or presentation."},
-   {'tag':'img','src':"../image/ga4_rawdata.jpg"},
-
-  {'tag':'div',
-   'class':'text',
-   'text':"To organize and restructure the raw data into an ideal format, I wrote some codes and made it a stats report generator."},
-   {'tag':'img','src':"../image/gen_report.gif"},
-     {'tag':'div',
-   'class':'text',
-   'text':"Other than the report in the textual form, I also generated some charts for reference. For example, the chart below shows the numbers of visitors from four difference social media sources. Two line charts shows the trend of Facebook and Instagram visitors."},
-   {'tag':'img','src':"../image/chart_1.jpg"},
-    {'tag':'div',
-   'class':'text',
-   'text':"Also, we can put two sources together into one chart for comparison."},
-   {'tag':'img','src':"../image/chart_2.jpg"},
-   {'tag':'div',
-   'class':'text',
-   'text':"Besides the current ad performance, I display history performance as well. When we see the chart below, we notice the spike on 9/21. Then I can get back to the marketing team and show them the chart. By doing so, they can take a closer or even a qualitative look at their promotional action on 9/21."},
-    {'tag':'img','src':"../image/chart_3.jpg"},
-      {'tag':'div',
-   'class':'text',
-   'text':"More than just the number of visitors, I also calculate the number of conversions. The prerequisite condition of conversion track is the configuration and setting through Google Tag Manager. The number of people who sumbit a contact form or add our official LINE account will be recorded and counted as conversions."},
-   {'tag':'div',
-   'class':'text',
-   'text':"As the school is still expanding and developing, the number of conversions every week isn't too magnificent. Therefore, I use dots to present the stats."},
-   {'tag':'img','src':"../image/chart_4.jpg"},
-   {'tag':'div','class':'title','text':'Future Development'},
-   {'tag':'div',
-   'class':'text',
-   'text':"The existing design can satisfy the current needs for data visualization and the insight into promotional marketing. However, there will be more to add. I plan to integrate the frontend, build the RESTful API in the backend in order to enable my colleagues to interact with the data. That will be a different story and work in my portfolio."},
-
-{
-  'tag':'div',
-  'class':'title',
-  'text':"2. CSV Data Processing and Visualization"},
-  {'tag':'div',
-   'class':'text',
-   'text':"In many cases, data sets are stored in the CSV format. To visualise the data sets, we have to process the CSV format first and then retrieve what we need, either digital numbers or charts. Here are some examples of processing CSV data so as to display insights into an industry or a problem."},
-  {'tag':'div','class':'title','text':'Example 1 - Correlation between Majors and Salaries'},
-  {'tag':'img','src':"../image/p_5_1.jpg"},
-  {'tag':'div','class':'text','text':"Here is the CSV file listing salaries of different majors right after graduation and a period of time. There’re more than 40 majors, each of which is categorized either business, STEM, or HASS."},
-  {'tag':'div','class':'boldText','text':' - Critial Data Retrieval'}, 
-  {'tag':'div','class':'text','text':' -  Find College Major with Highest Starting Salaries'},
-  
-  {'tag':'div','class':'text','text':'Here we can use a single line of code to grab the highest starting salary.'},{'tag':'img','src':"../image/p_5_2.jpg"},
-   
-  {'tag':'div','class':'text','text':'Then we locate the place of that number and retrieve the Major column. By doing so, we can see which department generates the highest starting salary.'},  {'tag':'img','src':"../image/p_5_3.jpg"},
-    {'tag':'div','class':'text','text':'Likewise, we can also answers the questions like What college major has the highest mid-career salary? How much do graduates with this major earn? Which college major has the lowest starting salary and how much do graduates earn after university? Which college major has the lowest mid-career salary and how much can people expect to earn with this degree?'},
-    {'tag':'div','class':'text','text':'The way we process and retrieve the data depends on what questions are raised and what information is needed. Next, I am going to take another use case for example.'},
-  {'tag':'div','class':'title','text':'Example 2 - Popular Programming Languages over Time'},
-  {'tag':'img','src':"../image/p_5_4.jpg"},
-    {'tag':'div','class':'text','text':'Here is a CSV file documenting the number of posts tagged with a specific programming language on Stackflow over time. For example, according to the chart, in August 2008, there were 85 posts discussing the programming language C. Next, we are going to visualise the data. Below we can see a chart showing the popularity of Java over time.'},
-        {'tag':'img','src':"../image/p_5_5.jpg"},
-        {'tag':'div','class':'text','text':'And this is the popularity of Python ove time.'}, 
-     {'tag':'img','src':"../image/p_5_6.jpg"},
-     {'tag':'div','class':'text','text':'We can present the popularity of Python and Java in the same chart for comparision.'},   
-    {'tag':'img','src':"../image/p_5_7.jpg"},
-   {'tag':'div','class':'text','text':'Finally, we can visualize the popularity of every programming language.'},
-   {'tag':'img','src':"../image/p_5_8.jpg"},
-     {'tag':'div','class':'text','text':'Still, sometimes we do not need everything on the plate. We can just pick a few key languages to be presented on the chart. Next, I will take another use case for example.'},
-     {'tag':'div','class':'title','text':'Example 3 - Number of LEGO Bricks among Themes'},
-     {'tag':'div','class':'text','text':'On the LEGO official website (https://rebrickable.com/downloads/), there are CSV files sorting out the sales conditions of different themes, colors, bricks and so on. Here is a bar chart visualizing the number of sets of each theme.'},
-      {'tag':'img','src':"../image/p_5_9.jpg"},
-      {'tag':'div','class':'title','text':'Example 4 - Correlation between Tesla Google Search Popularity and Stock Prices'},
-      {'tag':'div','class':'text','text':'This is definitely one of the most interesting data visualization cases I have ever seen. It combines Google search and the stock prices.'},{'tag':'img','src':"../image/p_5_10.jpg"},
-      {'tag':'div','class':'text','text':'Though we need more information to see how Google Search and stock prices are related to each other, the chart helps us catch a glimpse of the influence of online popularity on the decision-making behaviors observed in the stock market.'},
     {
-      'tag':'div',
-      'class':'title',
-      'text':"3. Conclusion"},
-      
-    {'tag':'div','class':'text','text':'Many people think visualizing data is cool and fancy, but it all comes down to communicative strategies - the best way to deliever messages and insights to our customers. All in all, in this fast-pacing world, not only does the retrieval of priceless information make a company advantageous, but the ability to process and interpret data and information is the key to business success.'},
+  'tag':'div',
+  'class':'title',
+  'text':"A Little Wit to Boost User Experience"},
+  {'tag':'div',
+   'class':'text',
+   'text':"It's not an easy task to delve into the expertise of energy in the first place. In the development process, I got opportunities to learn the concepts from Leo. To transform the expertise into something people can play with on the website, I positioned myself as a user without any background knowledge. To start with it, I need a tutorial upon arrival, which was also the feedback from one of the local authority stakeholders."},
+   {'tag':'img','src':"../image/energy_tutorial.gif"},
+   {'tag':'div',
+   'class':'text',
+   'text':"While the data is loading, we smoothly transition users into the tutorial to keep them engaged. Once the data is fully loaded, they seamlessly finish the tutorial, ensuring they aren't left twiddling their thumbs during loading times. It's a clever UX workaround that keeps the user experience engaged."},
+   {'tag':'div','class':'title','text':'Making Difficult Operation Easy and Intuitive'},
+   {'tag':'img','src':"../image/energy_sidebar.gif"},
+    {'tag':'div',
+   'class':'text',
+   'text':"Learnability is a challenge. What may be the best way to help users get into the swing of the graphs quickly? Other than the tutorial, we added validation messages on the sidebar, which is the primary tool users interact with to render the desired graphs. Also, we added tooltips. By doing so, we give users easy access to detailed information without making the interface too packed."},
+    {'tag':'div','class':'title','text':'Simplify Complexity'},
+    {'tag':'div',
+   'class':'text',
+   'text':"Given the immense amount of data we're dealing with, our filtering function is quite intricate. We offer users the option to select local authorities for data comparison. To make the interface tidy and clean, I install a expandable dropdown for users to select local authorities."},
+   {'tag':'img','src':"../image/energy_local.gif"},
+   {
+    'tag':'div',
+    'class':'title',
+    'text':"Contribution to the Open Source"},
+    {'tag':'div',
+   'class':'text',
+   'text':"The web app will soon be hosted on"},
+   {'tag':'a','href':'https://energytransitionexplorer.uk','text':'energytransitionexplorer.uk'},
+   {'tag':'div',
+   'class':'text',
+   'text':"The current version (11/4/2024) is available at"},
+   {'tag':'a','href':"http://vincejim91126.pythonanywhere.com/",'text':'vincejim91126.pythonanywhere.com'},
+   {'tag':'div',
+   'class':'text',
+   'text':"The website contains the source code. The GitHub README.md contains all the information for both users and developers."},
 
-      ] 
+  ] 
 
  
 for (let i = 0; i < text.length; i++){
@@ -165,6 +99,12 @@ for (let i = 0; i < text.length; i++){
     let Element = document.createElement(text[i]['tag']);
     Element.setAttribute('src',text[i]['src']);
     Container.appendChild(Element);
+  }else if(text[i]['tag'] == 'a'){
+      let Element = document.createElement(text[i]['tag']);
+      Element.setAttribute('href',text[i]['href']);
+      Element.setAttribute('target','_blank');
+      Element.textContent = text[i].text
+      Container.appendChild(Element);
   }else{
   let Element = document.createElement(text[i]['tag'])
   Element.classList.add(text[i].class)
--- static/index.js
+++ static/index.js
@@ -1,23 +1,10 @@
 //Text Value
 const main_topic_txt="YUEH COLLECTION"
 const slogan_txt="Programming | UX"
-const first_heading_txt="7+ years in Cybersecurity | 3+ years in Python Lecturing"
+const first_heading_txt="7+ Years of Digital Product Development"
 const first_content_txt = "Working in the realm of high-tech empowers me to envision the boundless advantages of technology, enabling me to shape a product that precisely addresses customers' needs and seek out the transformative value I must generate to truly revolutionize the world."
 const second_heading_txt = "Life is short. We should not waste time on programming something useless."
-/*const third_heading_txt = "Your Data is Stolen Before You Know it"
-const third_content_txt = "It's too late when there's a system alarm. By the time it is triggered, your data has already been stolen. To resolve it, try these solutions:<p>1. <b>Zero Trust</b> - Trust no one. Always verify. Stick to the principle of least privilege on the need-to-know basis<br>2. <b>Real-time Monitor</b> - Oversee the system to detect abnormal activities<br>3. <b>Encryption</b> - Encrypt your file, Even if data is leaked, there's no way to decipher it"
-const news1_heading_txt = "AuthTron Security Key joined Microsoft"
-const news1_content_txt = "WiSECURE's self-developed security key, AuthTron, passed Microsoft Azure AD compatibility test last year. This month, Microsoft officially included AuthTron in the Azure AD password-less authentication."
-const news1_link_txt = "AuthTron Spec >"
-const news1_link_href = "https://wisecure-tech.com/media/attachments/2022/04/01/authtron-product-briefzh.pdf"
-const news2_heading_txt = "kvHSM FIPS 140-2 Level 3 Certified"
-const news2_content_txt = "kvHSM, a PCIe-based hardware security module, has been validated by TUV and NIST, certified as meeting FIPS 140-2 Level 3 security requirements. Development for meeting FIPS 140-3 certification is ongoing."
-const news2_link_txt = "kvHSM Spec >"
-const news2_link_href = "https://wisecure-tech.com/media/attachments/2022/08/11/kvhsm-20220811zh_compressed.pdf"
-const news3_heading_txt = "FileAegis Entering the Semiconductor Industry"
-const news3_content_txt = "A FileAegis deployment project was launched in the semiconductor industry. Intellectual properties should be kept confidential and never be revealed to competitors to comply with supply chain regulations."
-const news3_link_txt = "Video Intro >"
-const news3_link_href = "https://youtu.be/A0h59XwbhP4"*/
+
 
 //Important Elements
 const main_topic = document.getElementById('main_topic')
@@ -25,17 +12,7 @@ const slogan = document.getElementById('slogan')
 const first_heading = document.getElementById('first_heading')
 const first_content = document.getElementById('first_content')
 const second_heading = document.getElementById('second_heading')
-/*const third_heading = document.getElementById('third_heading')
-const third_content = document.getElementById('third_content')
-const news1_heading = document.getElementById('news1_heading')
-const news1_content = document.getElementById('news1_content')
-const news1_link = document.getElementById('news1_link')
-const news2_heading = document.getElementById('news2_heading')
-const news2_content = document.getElementById('news2_content')
-const news2_link = document.getElementById('news2_link')
-const news3_heading = document.getElementById('news3_heading')
-const news3_content = document.getElementById('news3_content')
-const news3_link = document.getElementById('news3_link')*/
+
 
 //Render Element Values
 main_topic.innerText = main_topic_txt;
@@ -43,20 +20,7 @@ slogan.innerText = slogan_txt;
 first_heading.innerText = first_heading_txt;
 first_content.innerText = first_content_txt;
 second_heading.innerText = second_heading_txt;
-/*third_heading.innerText = third_heading_txt;
-third_content.innerHTML = third_content_txt;
-news1_heading.innerText = news1_heading_txt;
-news1_content.innerText = news1_content_txt;
-news1_link.innerText = news1_link_txt;
-news1_link.href = news1_link_href;
-news2_heading.innerText = news2_heading_txt;
-news2_content.innerText = news2_content_txt;
-news2_link.innerText = news2_link_txt;
-news2_link.href = news2_link_href;
-news3_heading.innerText = news3_heading_txt;
-news3_content.innerText = news3_content_txt;
-news3_link.innerText = news3_link_txt;
-news3_link.href = news3_link_href;*/
+
 
 
 //Rendering portfolio photos
@@ -98,9 +62,9 @@ function render(){
 
 //Content here
 var data_array = [
-{"id":6,"image":"../image/ga_img_6.jpg","info":"UI/UX, Python, Data Visualization, Gephi","name":"UX, Frontend - Network Graph Visualization"},
-{"id":3,"image":"../image/ga_img_3.jpg","info":"UX/UI, React, Typescript, Python, Data Visualization","name":"Titan Project - All-in-one Business Infra"},
-{"id":5,"image":"../image/ga_img_5.jpg","info":"Google Analytics 4 API, Python Pandas, Matplotlib","name":"Python - Data Visualization/Processing"},
+{"id":6,"image":"../image/ga_img_6.jpg","info":"UI/UX, React, Python","name":"UX, Frontend - Knowledge Navigation Map"},
+{"id":3,"image":"../image/ga_img_3.jpg","info":"UX/UI, API, Frontend","name":"UX, Fullstack - All-in-one Business Infra"},
+{"id":5,"image":"../image/ga_img_5.jpg","info":"Python Dash Plotly","name":"UX, Fullstack - Energy Scenario Explorer"},
 {"id":4,"image":"../image/ga_img_4.jpg","info":"React, Typescript, Python Flask (RESTful API), Cryptography","name":"UX, Fullstack - Bitcoin Money Generator"},
 {"id":2,"image":"../image/ga_img_2.jpg","info":"HTML/CSS/Javascript, Python Flask (RESTful API), SQL, AWS","name":"Fullstack - E-commerce Website"},
 {"id":1,"image":"../image/ga_img_1.jpg","info":"UI/UX, Content Strategy, Graphic Design","name":"UX Design - Cybersecurity Website"},
